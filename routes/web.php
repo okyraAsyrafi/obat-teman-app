f087a04f585e3b67a87790f773486539bec17d3b
okyraAsyrafi <okyra19chan@gmail.com>
2025-11-13 12:22:38 +0700

    Add new routes for educational content on TB symptoms and treatment

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,10 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('layouts/landing');
+    return view('layouts.landing');
 });
+
+// routes/web.php
+Route::get('/edukasi/mengenal-gejala-tb', fn() => view('landing.mengenal-gejala-tb'));
+Route::get('/edukasi/pentingnya-minum-obat', fn() => view('landing.pentingnya-minum-obat'));
+Route::get('/edukasi/bernafas-lega-acbt', fn() => view('landing.bernafas-lega-acbt'));
